b19aa07ef8d6c88f71ae79ae7296535b3b37f1b0
Lumbril <32036622+Lumbril@users.noreply.github.com>
2024-09-27 19:16:35 +0300

    fix for read image from file_uploader

--- my_streamlit/streamlit_app.py
+++ my_streamlit/streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import cv2
+import numpy as np
 from model import face_equals
 
 
 def equals(first_face, second_face):
     if first_face and second_face:
-        cosine_dist = face_equals.get_cosine(cv2.imread(first_face), cv2.imread(second_face))
+        first_face_bytes = np.asarray(bytearray(first_face.read()), dtype=np.uint8)
+        second_face_bytes = np.asarray(bytearray(second_face.read()), dtype=np.uint8)
+        cosine_dist = face_equals.get_cosine(cv2.imdecode(first_face_bytes, 1), cv2.imdecode(second_face_bytes, 1))
 
         if cosine_dist > 0.6:
             st.text('Лица одинаковые')
--- my_streamlit/test_streamlit.py
+++ my_streamlit/test_streamlit.py
@@ -12,11 +12,3 @@ def test_empty():
 def test_equals():
     cosine_dist = face_equals.get_cosine(cv2.imread('images/конор 2.jpg'), cv2.imread('images/леброн 1.png'))
     assert cosine_dist < 0.6
-
-
-def test_equals_from_image_open():
-    img1 = Image('images/конор 2.jpg')
-    img2 = Image('images/леброн 1.png')
-
-    cosine_dist = face_equals.get_cosine(np.ndarray(img1), np.ndarray(img2))
-    assert cosine_dist < 0.6
